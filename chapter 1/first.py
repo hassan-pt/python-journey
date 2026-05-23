6c2e4ede3b05fecb6d66b6d7afcd25432a36310c
hassan <h31039556@gmail.com>
2026-05-24 05:26:15 +0800

    Add files via upload

---
+++ chapter 1/first.py
@@ -0,0 +1,7 @@
+print("Hello")
+#basic print statement
+# you need to know that if you want to print something you have to do print("") for strings
+# for something like int, float, also for boolean flags values you have to just do that
+print(9)
+print(99.9)
+print(True)
---
+++ chapter 1/moduleandcomments.py
@@ -0,0 +1,16 @@
+import pyjokes
+# first of all pip is the install package manager for pyhton
+# like for pyjokes library i have done
+# pip install pyjokes
+# pip install....... whatever you want to install
+# (by using ctrl froward / key )we can generate comments
+# here this actually prints random jokes because i have use pyjokes module by using pip
+joke = pyjokes.get_joke()
+print("printing jokes......")
+print(joke)
+"""
+Multi lines
+ commenting"""
+#  we can also do commenting
+#  likse selecr all 
+#  and do ctrl forward slash jokes external library
